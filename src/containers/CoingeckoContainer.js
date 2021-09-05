970e2de4253b81280fdc17666b11826d9a530b0b
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-05 19:35:00 +0100

    renders container

---
+++ src/containers/CoingeckoContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CoingeckoContainer = () => {
+
+
+    return (
+        <h2>I'm the coingecko container</h2>
+    )
+
+}
+
+
+
+
+export default CoingeckoContainer
